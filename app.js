7d1e6d285e5d1ae82e573576b271e21e8b00b693
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2018-08-24 12:50:41 -0300

    Adicionado módulo do socket.io e realizado refactoring do projeto

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 /* importar as configurações do servidor */
-var app = require('./config/server');
+const app = require('./config/server');
 
-/* parametrizar porta de escuta */
-app.listen(80, function(){
+/* parametrizar porta de escuta 
+* O server é colocado em uma variável pois teremos que atribuir ele para o Socket.io
+*/
+
+const server = app.listen(8080, function(){
     console.log('Servidor online');
-})
+});
+
+//O socket.io irá usar a mesma porta que a aplicação, ou seja, ele responde 2 protocolos diferentes na mesma porta
+const io = require('socket.io').listen(server);
+
+/**
+* Criar a coneção por websocket
+*/
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+    
+    socket.on('disconnect',() => {
+        console.log('User disconnected');
+    });
+});
